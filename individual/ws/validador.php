6872c2fc9b164a0d971117d9808f4a862094ec58
Carlos Convit <carlos.convit@gmail.com>
2013-09-02 15:00:24 -0500

    Se agreogo el metodo de cotizacion para carros nuevos

--- individual/ws/validador.php
+++ individual/ws/validador.php
@@ -14,7 +14,7 @@ session_start();
      if(isset($_POST["nombre"]) && isset($_POST["cedula"]) && isset($_POST["telefono"]) && isset($_POST["correo"]) &&
         isset($_POST["edad"]) && isset($_POST["sexo"]) && isset($_POST["civil"]) && isset($_POST["tipo"]) && 
         isset($_POST["marca"]) && isset($_POST["modelo"]) && isset($_POST["ano"]) && isset($_POST["ocupantes"]) && 
-        isset($_POST["cobertura"]) && isset($_POST["version"]) && isset($_POST["valor"])){
+        isset($_POST["cobertura"]) && isset($_POST["version"])){
         //&& isset($_POST["flota"])
         //FALTA valor, usado 
          
@@ -25,6 +25,9 @@ session_start();
      if(sizeof($array_flota) > 0){  
          
             $cotizacion=new cotizacion();
+            $parametros=new parametros();
+            $array_parametros=$parametros->find_all();
+            
 
             $cotizacion->nombre=$_POST["nombre"];
             $cotizacion->cedula=$_POST["cedula"];
@@ -33,41 +36,27 @@ session_start();
             $cotizacion->edad=$_POST["edad"];
             $cotizacion->sexo=$_POST["sexo"];
             $cotizacion->estado_civil=$_POST["civil"];
-            $cotizacion->tipo_carro=1;
+            $cotizacion->tipo_carro=$_POST["uso"];
             $cotizacion->car_marca=$_POST["marca"];
             $cotizacion->car_modelo=$_POST["modelo"];
-            $cotizacion->car_version=$_POST["version"];
-            $cotizacion->car_ano=$_POST["ano"];
             $cotizacion->car_ocupantes=$_POST["ocupantes"];
             $cotizacion->tipo_cobertura=$_POST["cobertura"];
             $cotizacion->id_flota=$_POST["flota"];
             $cotizacion->id_flota=1;
             $cotizacion->cr_time='NOW()';
             $cotizacion->ut_time='NOW()';
+            
+            if($_POST["tipo"] == "nuevo"){
+             $cotizacion->valor_INMA=$_POST["factura"];//Ver de donde sacamos este valor
+            $cotizacion->car_version="";
+            $cotizacion->car_ano=$array_parametros[1]->valor;;   
+
+            }else{
             $cotizacion->valor_INMA=$_POST["valor"];//Ver de donde sacamos este valor
- 
- 
-   /*
-            $cotizacion->nombre="Eduardo Convit";
-            $cotizacion->cedula="17427127";
-            $cotizacion->telefono="573124221042";
-            $cotizacion->email="carlos.convit@gmail.com";
-            $cotizacion->edad=27;
-            $cotizacion->sexo=1;
-            $cotizacion->estado_civil=1;
-            $cotizacion->tipo_carro=1;
-            $cotizacion->car_marca="FORD";
-            $cotizacion->car_modelo="FIESTA";
-            $cotizacion->car_version="LX";
-            $cotizacion->car_ano="2013";
-            $cotizacion->car_ocupantes=5;
-            $cotizacion->tipo_cobertura=2;
-            $cotizacion->id_flota=1;
-            $cotizacion->valor_INMA="256000";
-            $cotizacion->cr_time='NOW()';
-            $cotizacion->ut_time='NOW()';
-          */  
-            //BUSCAR VALOR INMA
+            $cotizacion->car_version=$_POST["version"];
+            $cotizacion->car_ano=$_POST["ano"];
+            }
+
             
             //Busqueda de la clasificacion
             $find_aseguradora=new find_aseguradora();
@@ -83,8 +72,6 @@ session_start();
             $find_aseguradora->get_coberturas($cotizacion->tipo_cobertura,$res_clasificacion);
             //var_dump($res_clasificacion);
             
-            $parametros=new parametros();
-            $array_parametros=$parametros->find_all();
             
             $solicitud=new solicitud();
             $solicitud->cotizacion=$cotizacion;
