4bfae3b69c56660de000d8284643bbbe0842a17a
riverarcarlosm <riverarcarlosm@gmail.com>
2013-09-03 22:19:10 -0430

    pagina de confirmacion, se arreglaron los estilos del menu footer

---
+++ individual/confirmacion.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>Aon - Gracias por cotizar con Aon</title>
+<link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
+<link href="css/normalize.css" rel="stylesheet" type="text/css">
+<link href="css/style.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+<div id="contenedor">
+	<div id="supper" class="separator"></div>
+	<div id="header">
+		<div class="center">
+			<p class="menu"><a class="current" href="index.php">Inicio</a> <span>│</span> <a href="terminos.html">Términos de Uso</p>
+			<p><a href="index.php"><img src="img/logo.png"></a></p>
+			<p> <span>Aon Risk Services Venezuela Corretaje de Seguros, C.A. | RIF J-00067607-0 | Inscripción SAA N 102.</span> <span style="float: right; margin-top: -9px">Facilidad disponible para empleados de <img src="img/gm.png" width="24" height="24"></span> </p>
+			<div class="exclusivo"></div>
+		</div>
+	</div>
+	<div id="contenido">
+		<div class="center">
+      <p>Gracias por cotizar con nosotros. En la brevedad un Ejecutivo de Atención al Cliente se comunicará con usted. Si desea contactarnos directamente lo puede hacer a través de los teléfonos 212 4009324 | 212 4009322. o por correo electrónico a la cuenta ve.solicito.cotizacion@aon.com</p>
+    </div>
+	</div>
+	<div id="mensaje">
+		<div class="center"> <span>&nbsp;</span> <span id="error" class="red pull-rigth hide"></span></div>
+	</div>
+	<div id="footer">
+		<div class="center">
+			<div class="separator"></div>
+			<div id="legal">
+        <input type="button" id="btn-cotizar" class="boton boton-footer" value="Volver a cotizar" style="width: 145px; background-position: 30px 0px">
+			</div>
+		</div>
+	</div>
+</div>
+<script src="../plugins/jquery-1.10.2.min.js"></script> 
+<script src="js/main.js"></script>
+</body>
+</html>
