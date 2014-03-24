370719c7f22b2f116ff7ec8aab79ef27383cc925
Carlos Rivera <riverarcarlosm@gmail.com>
2014-03-24 10:14:04 -0430

    access icon position fix

--- flota/index.php
+++ flota/index.php
@@ -1,50 +1,64 @@
 <!DOCTYPE html>
 <html>
-  <head>
-		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-		<title>Aon - Crear flota</title>
-		<link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
-		<link href="../plugins/css/jquery-ui-1.10.4.custom.min.css" rel="stylesheet" type="text/css">
-		<link href="../plugins/css/normalize.css" rel="stylesheet" type="text/css">
-		<link href="css/style.css" rel="stylesheet" type="text/css">
-  </head>
-  <body>
-    <div id="container">
-      <div id="header">
-        <a href="index.php"><img id="logo" src="img/logo.png"></a>
-        <div id="top-nav"></div>
-      </div>
-      <div id="content">
-        <div id="left-nav">
-          <ul>
-            <li><a href="clientes.php">Clientes</a></li>
-            <li><a href="seguros.php">Seguros</a></li>
-            <li><a href="coberturas.php">Coberturas</a></li>
-            <li><a href="convenios.php">Convenios</a></li>
-            <li><a href="flotas.php">Flotas</a></li>
-            <li style="border: none;"><a href="cotizaciones.php">Cotizaciones</a></li>
-          </ul>
-        </div>
-        <div id="main">
-          <div class="access">
-            <div class="access-icon-right pull-left">
-              <a href="flotas.php" class="access-icon new-cotizacion"></a>
-              <a href="flotas.php" class="access-title">Nueva flota</a>
-            </div>
-            <div class="access-icon-right pull-left">
-              <a href="cotizaciones.php" class="access-icon new-flota"></a>
-              <a href="cotizaciones.php" class="access-title">Nueva cotización</a>
-            </div>
-            <div class="access-icon-panel pull-left">
-              <a href="convenios.php" class="access-icon view-cotizacion"></a>
-              <a href="convenios.php" class="access-title">Nuevo convenio</a>
-            </div>          
-          </div>
-        </div>
-      </div>
-    </div>  
-    <div id="footer"></div>
-    <script src="../plugins/js/jquery-1.10.2.min.js"></script>  
-    <script src="../plugins/js/jquery-ui-1.10.4.custom.min.js"></script>
-    <script src="js/main.js"></script>
-  </body>
+
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>Aon - Crear flota</title>
+	<link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
+	<link href="../plugins/css/jquery-ui-1.10.4.custom.min.css" rel="stylesheet" type="text/css">
+	<link href="../plugins/css/normalize.css" rel="stylesheet" type="text/css">
+	<link href="css/style.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+	<div id="container">
+		<div id="header">
+			<a href="index.php">
+				<img id="logo" src="img/logo.png">
+			</a>
+			<div id="top-nav"></div>
+		</div>
+		<div id="content">
+			<div id="left-nav">
+				<ul>
+					<li>
+						<a href="clientes.php">Clientes</a>
+					</li>
+					<li>
+						<a href="seguros.php">Seguros</a>
+					</li>
+					<li>
+						<a href="coberturas.php">Coberturas</a>
+					</li>
+					<li>
+						<a href="convenios.php">Convenios</a>
+					</li>
+					<li>
+						<a href="flotas.php">Flotas</a>
+					</li>
+					<li style="border: none;">
+						<a href="cotizaciones.php">Cotizaciones</a>
+					</li>
+				</ul>
+			</div>
+			<div class="access">
+				<div class="access-icon-right pull-left">
+					<a href="flotas.php" class="access-icon new-cotizacion"></a>
+					<a href="flotas.php" class="access-title">Nueva flota</a>
+				</div>
+				<div class="access-icon-right pull-left">
+					<a href="cotizaciones.php" class="access-icon new-flota"></a>
+					<a href="cotizaciones.php" class="access-title">Nueva cotización</a>
+				</div>
+				<div class="access-icon-panel pull-left">
+					<a href="convenios.php" class="access-icon view-cotizacion"></a>
+					<a href="convenios.php" class="access-title">Nuevo convenio</a>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div id="footer"></div>
+	<script src="../plugins/js/jquery-1.10.2.min.js"></script>
+	<script src="../plugins/js/jquery-ui-1.10.4.custom.min.js"></script>
+	<script src="js/main.js"></script>
+</body>
